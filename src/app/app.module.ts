c41f4d528ac8c488e87fe11d5c89a56cb578f18b
steven <scoaila@proton.me>
2025-01-19 18:25:23 -0500

    dbz added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DbzModule } from './dbz/dbz.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    CounterModule,
-    HeroesModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DbzModule, CounterModule, HeroesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

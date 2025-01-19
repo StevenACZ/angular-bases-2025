d5b6ba250f9745f33191d795a1b054e595e39566
steven <scoaila@proton.me>
2025-01-19 18:43:17 -0500

    Add character list and add character components

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
 
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
   exports: [MainPageComponent],
   imports: [CommonModule],
 })

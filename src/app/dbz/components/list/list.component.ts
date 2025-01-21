3fad7b02d48df1d0b85fc07522e0cccf830bfaaf
steven <scoaila@proton.me>
2025-01-21 15:10:13 -0500

    Implement character list display with dynamic data binding

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{ name: 'Vegeta', power: 9500 }];
+}
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Character } from '../interfaces/character.interface';
+
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  constructor() {}
+  public characters: Character[] = [
+    { name: 'Piccolo', power: 8000 },
+    { name: 'Gohan', power: 8500 },
+    { name: 'Frieza', power: 12000 },
+    { name: 'Cell', power: 11000 },
+    { name: 'Majin Buu', power: 13000 },
+    { name: 'Broly', power: 14000 },
+    { name: 'Beerus', power: 15000 },
+    { name: 'Whis', power: 16000 },
+    { name: 'Jiren', power: 17000 },
+    { name: 'Hit', power: 12500 },
+  ];
 }

23ae75cda4241567da4e78b7cad009a5fad72c92
steven <scoaila@proton.me>
2025-01-21 15:28:19 -0500

    Implement character addition functionality with two-way data binding

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css'],
 })
-export class AddCharacterComponent {}
+export class AddCharacterComponent {
+  public newCharacter: Character = {
+    name: 'ABC',
+    power: 0,
+  };
+
+  emitNewCharacter(): void {
+    console.log(this.newCharacter);
+  }
+}
